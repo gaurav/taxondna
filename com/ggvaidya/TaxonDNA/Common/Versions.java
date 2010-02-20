88233a41727018e5d5aa676b022da495b13358d1
gaurav <gaurav@ggvaidya.com>
2010-02-20 18:10:34 +0800

    Updated version numbers.

--- com/ggvaidya/TaxonDNA/Common/Versions.java
+++ com/ggvaidya/TaxonDNA/Common/Versions.java
@@ -32,6 +32,6 @@ public class Versions {
 	 * Returns the common version string for the entire TaxonDNA package. 
 	 */
 	public static String getTaxonDNA() {
-		return "1.6.3-dev10-Feb-13-2010-DO-NOT-USE";
+		return "1.6.3-dev11-Feb-20-2010";
 	}
 }
