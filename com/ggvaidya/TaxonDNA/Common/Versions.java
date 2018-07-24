c8d0d5675d0d0f7ec02807e0c8021058c84ca661
Gaurav Vaidya <gaurav@ggvaidya.com>
2018-07-23 23:19:37 -0400

    Updated version to 1.9-dev1.

--- com/ggvaidya/TaxonDNA/Common/Versions.java
+++ com/ggvaidya/TaxonDNA/Common/Versions.java
@@ -2,7 +2,7 @@
  * Versions contains various constants and values used by TaxonDNA
  * in general.
  *
- * @author Gaurav Vaidya gaurav@ggvaidya.com 
+ * @author Gaurav Vaidya gaurav@ggvaidya.com
  */
 
 /*
@@ -22,16 +22,16 @@
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
- * 
+ *
  */
 
 package com.ggvaidya.TaxonDNA.Common;
 
 public class Versions {
 	/**
-	 * Returns the common version string for the entire TaxonDNA package. 
+	 * Returns the common version string for the entire TaxonDNA package.
 	 */
 	public static String getTaxonDNA() {
-		return "1.8";
+		return "1.9-dev1";
 	}
 }
